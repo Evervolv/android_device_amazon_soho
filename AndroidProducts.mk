90ab6a2de00ee3ecec39c629cf9f11b47d7eabac
Saleem Rashid <dev@saleemrashid.com>
2015-12-04 13:00:48 +0000

    full_soho: Initial commit

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/device_soho.mk
+    $(LOCAL_DIR)/full_soho.mk
--- cm.mk
+++ cm.mk
@@ -8,7 +8,7 @@ PRODUCT_RELEASE_NAME := soho
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/amazon/soho/device_soho.mk)
+$(call inherit-product, device/amazon/soho/full_soho.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := soho
