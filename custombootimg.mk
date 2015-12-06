7a355b22cecbd7f1bbd07d8e34a8d5fd7b91cd68
Saleem Rashid <dev@saleemrashid.com>
2015-12-06 17:54:24 +0000

    custombootimg: Build compatible recovery images

---
+++ custombootimg.mk
@@ -0,0 +1,12 @@
+SOHO_HEADER_DATA := '\x50\x03\x00\x00\x00\x25\xe4\x00'
+
+$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
+		$(recovery_ramdisk) \
+		$(recovery_kernel)
+	@echo -e ${PRT_IMG}"----- Making recovery image ------"${CL_RST}
+	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@.std
+	$(hide) dd if=/dev/zero of=$@ bs=1024 count=1
+	$(hide) echo -ne $(SOHO_HEADER_DATA) | dd of=$@ bs=52 seek=1 conv=notrunc
+	$(hide) cat $@.std >> $@
+	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
+	@echo -e ${PRT_IMG}"----- Made recovery image: $@ --------"${CL_RST}
