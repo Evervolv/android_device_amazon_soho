0ba258cf6d42334dc2301d5bf20c215079d31eb6
Saleem Rashid <dev@saleemrashid.com>
2015-12-08 19:36:20 +0000

    BoardConfig: Use standard 848 B certificates
    Compatible with stock images

--- custombootimg.mk
+++ custombootimg.mk
@@ -5,7 +5,7 @@ $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
 		$(recovery_kernel)
 	@echo -e ${PRT_IMG}"----- Making recovery image ------"${CL_RST}
 	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@.std
-	$(hide) dd if=/dev/zero of=$@ bs=1024 count=1
+	$(hide) dd if=/dev/zero of=$@ bs=848 count=1
 	$(hide) echo -ne $(SOHO_HEADER_DATA) | dd of=$@ bs=52 seek=1 conv=notrunc
 	$(hide) cat $@.std >> $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
